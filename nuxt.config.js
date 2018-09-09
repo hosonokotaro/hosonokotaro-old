cd9da589795dc8cbd42a65d3af55d19176de074a
hosono kotaro <hosono1985@gmail.com>
2018-09-09 19:25:41 +0900

    add nuxt

---
+++ nuxt.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'hosonokotaro',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      { src: '/__/firebase/5.4.2/firebase-app.js', body: true },
+      { src: '/__/firebase/init.js', body: true }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
